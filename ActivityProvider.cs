6bcc0695210935b9f99f292cf0e3808c44aaefb5
Igor Teixeira <al75038@alunos.utad.pt>
2021-11-29 08:39:38 +0000

    Add project files.

---
+++ ActivityProvider.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace apAPI
+{
+    public class ActivityProvider
+    {
+        public string ActivityID { get; set; }
+
+        public string InveniRAstdID { get; set; }
+
+        public List<string> Json_params { get; set; }
+    }
+}
